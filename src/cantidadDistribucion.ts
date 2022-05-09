f4f9583cb6eb749cd46e729c0b3f2f2f0722260e
nahuel benavides paredes <104174231+nahuel-benavides@users.noreply.github.com>
2022-05-09 18:54:42 -0300

    corregido

--- src/cantidadDistribucion.ts
+++ src/cantidadDistribucion.ts
@@ -3,14 +3,19 @@ let numPositivos: number = 0;
 let numTotales: number = 0;
 let porcentajes: number = 0;
 
-while (numero != 0) {
+while (numero !== 0) {
   if (numero > 0) {
     numPositivos++;
   }
   numTotales++;
-  numero: number = Number(prompt("ingrese otro numero"));
+  numero = Number(prompt("ingrese numero"));
 }
-if(numTotales>0){
-  porcentajes = numPositivos*100/numTotales;
-  console.log(numPositivos; "numero positivos" ; porcentajes; "porcentaje total" ) 
+if (numTotales > 0) {
+  porcentajes = (numPositivos * 100) / numTotales;
+  console.log(
+    numPositivos,
+    "numero positivos",
+    porcentajes,
+    "porcentaje total"
+  );
 }
