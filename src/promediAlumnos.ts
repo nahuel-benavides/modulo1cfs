228b90da8613b23ddffb9c9fc88c8c22fe96bc3d
nahuel benavides paredes <104174231+nahuel-benavides@users.noreply.github.com>
2022-05-31 21:55:31 -0300

    actualizacion

--- src/promediAlumnos.ts
+++ src/promediAlumnos.ts
@@ -1,33 +1,32 @@
 function PromedioAlumno(indice: number): number {
-  for (indice = 0; indice < numeroAlumnos; indice++) {
+  for (indice = 0; indice < alumnos[]; indice++) {
     let suma: number = (primTrim + segTrim + terTrim) / 3;
     return suma;
   }
 }
 
-function posicionAlumno(nombre: string): number {
-  for (indice = 0; indice < numeroAlumnos; indice++) {
-    let nombre: string = prompt("ingrese nombre del alumno");
-    let posicion: number = -1;
-    if (nombre === alumnos[numeroAlumnos]) {
-      posicion = indice;
-      return posicion;
+function posicionAlumno(arregloAlumnos:number[]): number {
+  for (let indice:number = 0; indice< alumnos.length; indice++) {
+    let posicion:number = 0;
+    arregloAlumnos[indice] = posicion; 
+return posicion; 
     }
   }
-}
+
 
 let numeroAlumnos: number = Number(prompt("ingrese el numero de alumnos"));
 let alumnos: string[] = new Array(numeroAlumnos);
-let indice: number = 0;
-let primTrim: number = 0;
-let segTrim: number = 0;
-let terTrim: number = 0;
+let primTrim: number = new Array(numeroAlumnos);
+let segTrim: number = new Array(numeroAlumnos);
+let terTrim: number = new Array(numeroAlumnos);
 
-for (indice = 0; indice < numeroAlumnos; indice++) {
-  alumnos[indice] = prompt("ingrese el nombre del alumno");
-  primTrim = Number(prompt("ingrese la nota final del primer trimestre"));
-  segTrim = Number(prompt("ingrese la nota final del segundo trimestre"));
-  terTrim = Number(prompt("ingrese la nota final del tercer trimestre"));
+
+for (let indice:number = 0; indice < alumnos.length; indice++) {
+  alumnos[indice] = prompt("ingrese el nombre del alumno " + indice );
+  primTrim = Number(prompt("ingrese la nota final del primer trimestre de " + alumnos[indice]));
+  segTrim = Number(prompt("ingrese la nota final del segundo trimestre de " + alumnos[indice]));
+  terTrim = Number(prompt("ingrese la nota final del tercer trimestre de " + alumnos[indice]));
 }
 
-console.log(PromedioAlumno(posicionAlumno(alumnos[indice])));
+console.log(posicionAlumno(alumnos[indice]));
+
